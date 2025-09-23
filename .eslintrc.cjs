6e8a1c8f71ce2e0cc937cb04cb99f0ddb1ac9522
Ratmubaba <141784203+kdt1988@users.noreply.github.com>
2025-09-23 08:50:14 +0200

    Create  .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+ root: true,
+ env: { browser: true, es2022: true, node: true },
+ parser: '@typescript-eslint/parser',
+ plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+ extends: [
+ 'eslint:recommended',
+ 'plugin:@typescript-eslint/recommended',
+ 'plugin:react/recommended',
+'plugin:react-hooks/recommended',
+ ],
+ settings: { react: { version: 'detect' } },
+ };
